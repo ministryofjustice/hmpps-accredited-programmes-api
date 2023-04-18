c941d794a61f474eba416bc1325fa7bbc15462ff
Alvaro Martin <alvaroetsii@gmail.com>
2023-04-18 14:47:29 +0100

    Changed test endpoint to return json so that it matches the api spec

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/controller/HelloController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/controller/HelloController.kt
@@ -3,10 +3,11 @@ package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.controller
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.api.HelloApiDelegate
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.api.model.Hello
 
 @Service
 class HelloController : HelloApiDelegate {
-  override fun helloGet(): ResponseEntity<String> {
-    return ResponseEntity.ok("Welcome to the Accredited Programs API")
+  override fun helloGet(): ResponseEntity<Hello> {
+    return ResponseEntity.ok(Hello(message = "Welcome to the Accredited Programs API"))
   }
 }
