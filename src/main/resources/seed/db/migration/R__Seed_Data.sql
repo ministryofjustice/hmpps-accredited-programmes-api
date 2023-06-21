fa54f84ad99683609f5d8bc6ed181b70aab1d278
Peter Bell <peter.bell@digital.justice.gov.uk>
2023-06-21 11:23:09 +0100

    Add a Spring Boot profile 'seed' that loads seed data into the application's database.
    The seed profile is defined by application-seed.yml.
    The seed data is loaded into the database using a flyway repeatable migration at src/main/resources/seed/db/migration/R__Seed_Data.sql
    Finally, the docker compose configuration file docker-compose.yml has been updated to launch the hmpps-accredited-programmes-api image container with the seed profile enabled. This means that application can be started and populated with seed data using 'docker-compose up'.

---
+++ src/main/resources/seed/db/migration/R__Seed_Data.sql
@@ -0,0 +1,46 @@
+DELETE from course_audience;
+DELETE from offering;
+DELETE from prerequisite;
+DELETE from audience;
+DELETE from course;
+
+INSERT INTO audience(audience_id, audience_value)
+VALUES ('7fffcc6a-11f8-4713-be35-cf5ff1aee517', 'Sexual violence'),
+       ('7fffcc6a-11f8-4713-be35-cf5ff1aee518', 'Extremism offence'),
+       ('7fffcc6a-11f8-4713-be35-cf5ff1aee519', 'Gang offence'),
+       ('7fffcc6a-11f8-4713-be35-cf5ff1aee510', 'Violent offence'),
+       ('7fffcc6a-11f8-4713-be35-cf5ff1aee511', 'Intimate partner violence');
+
+INSERT INTO course(course_id, name, description)
+VALUES ('d3abc217-75ee-46e9-a010-368f30282367', 'Lime Course', 'Explicabo exercitationem non asperiores corrupti accusamus quidem autem amet modi. Mollitia tenetur fugiat quo aperiam quasi error consectetur. Fugit neque rerum velit rem laboriosam. Atque nostrum quam aspernatur excepturi laborum harum officia eveniet porro.'),
+       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'Azure Course', 'Similique laborum incidunt sequi rem quidem incidunt incidunt dignissimos iusto. Explicabo nihil atque quod culpa animi quia aspernatur dolorem consequuntur.'),
+       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'Violet Course', 'Tenetur a quisquam facilis amet illum voluptas error. Eaque eum sunt odit dolor voluptatibus eius sint impedit. Illo voluptatem similique quod voluptate laudantium. Ratione suscipit tempore amet autem quam dolorum. Necessitatibus tenetur recusandae aliquam recusandae temporibus voluptate velit similique fuga. Id tempora doloremque.');
+
+INSERT INTO prerequisite(course_id, name, description)
+VALUES ('d3abc217-75ee-46e9-a010-368f30282367', 'Setting', 'Custody'),
+       ('d3abc217-75ee-46e9-a010-368f30282367', 'Risk criteria', 'High ESARA/SARA/OVP, High OGRS'),
+       ('d3abc217-75ee-46e9-a010-368f30282367', 'Criminogenic needs', 'Relationships, Thinking and Behaviour, Attitudes, Lifestyle'),
+       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'Setting', 'Custody'),
+       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'Risk criteria', 'High ESARA/SARA/OVP, High OGRS'),
+       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'Criminogenic needs', 'Relationships, Thinking and Behaviour, Attitudes, Lifestyle'),
+       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'Setting', 'Custody'),
+       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'Risk criteria', 'High ESARA/SARA/OVP, High OGRS'),
+       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'Criminogenic needs', 'Relationships, Thinking and Behaviour, Attitudes, Lifestyle');
+
+INSERT INTO offering(offering_id, course_id, organisation_id, contact_email)
+VALUES ('7fffcc6a-11f8-4713-be35-cf5ff1aee517', 'd3abc217-75ee-46e9-a010-368f30282367', 'MDI', 'nobody-mdi@digital.justice.gov.uk'),
+       ('790a2dfe-7de5-4504-bb9c-83e6e53a6537', 'd3abc217-75ee-46e9-a010-368f30282367', 'BWN', 'nobody-bwn@digital.justice.gov.uk'),
+       ('39b77a2f-7398-4d5f-b744-cdcefca12671', 'd3abc217-75ee-46e9-a010-368f30282367', 'BXI', 'nobody-bxi@digital.justice.gov.uk'),
+
+       ('20f3abc8-dd92-43ae-b88e-5797a0ad3f4b', '28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'OCC', 'nobody-iry@digital.justice.gov.uk'),
+
+       ('b328ebc8-1f7b-4236-b4ac-30f50b43a92d', '1811faa6-d568-4fc4-83ce-41118b90242e', 'BWN', 'nobody-bwn@digital.justice.gov.uk');
+
+INSERT INTO course_audience (course_id, audience_id)
+VALUES ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', '7fffcc6a-11f8-4713-be35-cf5ff1aee517'),
+       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', '7fffcc6a-11f8-4713-be35-cf5ff1aee511'),
+       ('d3abc217-75ee-46e9-a010-368f30282367', '7fffcc6a-11f8-4713-be35-cf5ff1aee518'),
+       ('d3abc217-75ee-46e9-a010-368f30282367', '7fffcc6a-11f8-4713-be35-cf5ff1aee519'),
+       ('d3abc217-75ee-46e9-a010-368f30282367', '7fffcc6a-11f8-4713-be35-cf5ff1aee510'),
+       ('1811faa6-d568-4fc4-83ce-41118b90242e', '7fffcc6a-11f8-4713-be35-cf5ff1aee518'),
+       ('1811faa6-d568-4fc4-83ce-41118b90242e', '7fffcc6a-11f8-4713-be35-cf5ff1aee519');
