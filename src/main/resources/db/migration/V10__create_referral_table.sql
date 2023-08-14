19bee7053c0ad7cc3bdf5f76e57030df75c6400c
Peter Bell <peter.bell@digital.justice.gov.uk>
2023-08-14 15:46:05 +0100

    Fix inadvertent incompatibility in referral PK and FK column types.
    Change column type from TEXT to UUID

--- src/main/resources/db/migration/V10__create_referral_table.sql
+++ src/main/resources/db/migration/V10__create_referral_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS referral
 (
-    referral_id TEXT NOT NULL,
-    offering_id TEXT NOT NULL,
+    referral_id UUID NOT NULL,
+    offering_id UUID NOT NULL,
     prison_number TEXT NOT NULL,
     referrer_id TEXT NOT NULL,
     CONSTRAINT referral_pk PRIMARY KEY (referral_id),
