15ec04d431a382ed184f189a230b23c9a2ce5df2
Peter Bell <peter.bell@digital.justice.gov.uk>
2023-09-25 11:41:22 +0100

    Refactoring. Simply the annotations on the `RepositoryTest` base class.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/shared/jpa/RepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/shared/jpa/RepositoryTest.kt
@@ -2,26 +2,24 @@ package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.shared.jpa
 
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.authentication.TestingAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.User
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.transaction.TestTransaction
 import org.springframework.test.jdbc.JdbcTestUtils
+import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.shared.TEST_USER_NAME
 
 private const val BASE_PACKAGE = "uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi"
 
-private const val COURSE_PACKAGE = "$BASE_PACKAGE.course"
-
-@DataJpaTest
-@ContextConfiguration(classes = [RepositoryTest::class])
-@ComponentScan(basePackages = [COURSE_PACKAGE, "$BASE_PACKAGE.shared"])
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase
 @EnableJpaRepositories(basePackages = [BASE_PACKAGE])
 @EntityScan(basePackages = [BASE_PACKAGE])
 @ActiveProfiles("test")
