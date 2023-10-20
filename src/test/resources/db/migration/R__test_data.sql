b44ae45519f8dec8e6d2b9938de284dedeafda87
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2023-10-20 14:08:03 +0100

    Add missing pact tests and reenabling them

--- src/test/resources/db/migration/R__test_data.sql
+++ src/test/resources/db/migration/R__test_data.sql
@@ -1,32 +1,32 @@
-INSERT INTO audience(audience_id, audience_value)
-VALUES ('7fffcc6a-11f8-4713-be35-cf5ff1aee517', 'Sexual violence');
+
 
 INSERT INTO course(course_id, identifier, name, description, alternate_name)
 VALUES ('d3abc217-75ee-46e9-a010-368f30282367', 'LC', 'Lime Course', 'Explicabo exercitationem non asperiores corrupti accusamus quidem autem amet modi. Mollitia tenetur fugiat quo aperiam quasi error consectetur. Fugit neque rerum velit rem laboriosam. Atque nostrum quam aspernatur excepturi laborum harum officia eveniet porro.', 'LC'),
-       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'AC', 'Azure Course', 'Similique laborum incidunt sequi rem quidem incidunt incidunt dignissimos iusto. Explicabo nihil atque quod culpa animi quia aspernatur dolorem consequuntur.', 'AC++'),
-       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'VC', 'Violet Course', 'Tenetur a quisquam facilis amet illum voluptas error. Eaque eum sunt odit dolor voluptatibus eius sint impedit. Illo voluptatem similique quod voluptate laudantium. Ratione suscipit tempore amet autem quam dolorum. Necessitatibus tenetur recusandae aliquam recusandae temporibus voluptate velit similique fuga. Id tempora doloremque.', null);
+       ('1811faa6-d568-4fc4-83ce-41118b902421', 'SC', 'Super Course', 'corona volva conturbo suscipit civis.', 'AC++'),
+       ('d3abc217-75ee-46e9-a010-368f30282368', 'RC', 'RAPID Course', 'corona volva conturbo suscipit civis.', NULL),
+       ('d3abc217-75ee-46e9-a010-368f30282369', 'CC', 'Custom Course', 'corona custom suscipit civis.', 'AC++'),
+       ('d3abc217-75ee-46e9-a010-368f30282370', 'TC', 'Temp Course', 'Temp custom suscipit civis.', 'AC++');
 
-INSERT INTO prerequisite(course_id, name, description)
-VALUES ('d3abc217-75ee-46e9-a010-368f30282367', 'Setting', 'Custody'),
-       ('d3abc217-75ee-46e9-a010-368f30282367', 'Risk criteria', 'High ESARA/SARA/OVP, High OGRS'),
-       ('d3abc217-75ee-46e9-a010-368f30282367', 'Criminogenic needs', 'Relationships, Thinking and Behaviour, Attitudes, Lifestyle'),
-       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'Setting', 'Custody'),
-       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'Risk criteria', 'High ESARA/SARA/OVP, High OGRS'),
-       ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'Criminogenic needs', 'Relationships, Thinking and Behaviour, Attitudes, Lifestyle'),
-       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'Setting', 'Custody'),
-       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'Risk criteria', 'High ESARA/SARA/OVP, High OGRS'),
-       ('1811faa6-d568-4fc4-83ce-41118b90242e', 'Criminogenic needs', 'Relationships, Thinking and Behaviour, Attitudes, Lifestyle');
+INSERT INTO course_participation (course_participation_id, prison_number, course_name, source, detail, location, type, outcome_status, year_started, year_completed, created_by_username, created_date_time, last_modified_by_username, last_modified_date_time)
+VALUES
+    ('1c0fbebe-7768-4dbe-ae58-6036183dbeff', 'CpzK5Kx', 'Silver Course', NULL, '', 'Schulist End', 'COMMUNITY', 'INCOMPLETE', 2021, NULL, 'Celia Feest', '2023-10-05T12:48:20', NULL, NULL),
+    ('9372880f-95ba-4cde-a71a-01e2a06a0644', 'A1234AA', 'Orchid Course', 'deaden', 'Xiphias ustilo solutio crux. Velociter corrumpo una usitas.', 'Schulist End', 'COMMUNITY', 'INCOMPLETE', 2021, NULL, 'Germaine Luettgen', '2023-10-19T09:40:39', NULL, NULL),
+    ('0cff5da9-1e90-4ee2-a5cb-94dc49c4b004', 'A1234AA', 'Green Course', 'squirrel', 'Cimentarius tactus vitium armarium adnuo tibi tricesimus volo. Decet ad adimpleo. Alienus tantillus tibi caecus. Utilis quia beatae velociter quos comitatus caelestis. Currus canis decimus comparo veniam.', 'Schulist End', 'COMMUNITY', 'INCOMPLETE', 2021, 1985, 'Carmelo Conn', '2023-10-11T13:11:06', NULL, NULL),
+    ('eb357e5d-5416-43bf-a8d2-0dc8fd92162e', 'A1234AA', 'Red Course', 'deaden', 'Absum calamitas capio. Aro defero vester temporibus venia utique vomica vito suppono. Acervus armarium officia confido speciosus vere texo laborum corrigo excepturi. Succurro deorsum utpote peior subito theologus. Tersus solvo suadeo vomica turbo causa cursus tolero celo quam.', 'Schulist End', 'CUSTODY', 'INCOMPLETE', 2021, NULL, 'Joanne Hamill', '2023-09-21 23:45:12', NULL, NULL),
+    ('eb357e5d-5416-43bf-a8d2-0dc8fd92162f', 'B1234BB', 'Red Course', 'deaden', 'Absum calamitas capio. Aro defero vester temporibus venia utique vomica vito suppono. Acervus armarium officia confido speciosus vere texo laborum corrigo excepturi. Succurro deorsum utpote peior subito theologus. Tersus solvo suadeo vomica turbo causa cursus tolero celo quam.', 'Schulist End', 'CUSTODY', 'INCOMPLETE', 2021, 1985, 'Joanne Hamill', '2023-09-21 23:45:12', NULL, NULL);
 
 INSERT INTO offering(offering_id, course_id, organisation_id, contact_email, secondary_contact_email)
-VALUES ('7fffcc6a-11f8-4713-be35-cf5ff1aee517', 'd3abc217-75ee-46e9-a010-368f30282367', 'MDI', 'nobody-mdi@digital.justice.gov.uk', 'nobody2-mdi@digital.justice.gov.uk'),
-       ('790a2dfe-7de5-4504-bb9c-83e6e53a6537', 'd3abc217-75ee-46e9-a010-368f30282367', 'BWN', 'nobody-bwn@digital.justice.gov.uk', null),
-       ('39b77a2f-7398-4d5f-b744-cdcefca12671', 'd3abc217-75ee-46e9-a010-368f30282367', 'BXI', 'nobody-bxi@digital.justice.gov.uk', null),
+VALUES
+        ('790a2dfe-7de5-4504-bb9c-83e6e53a6537', 'd3abc217-75ee-46e9-a010-368f30282367', 'BWN', 'nobody-bwn@digital.justice.gov.uk', null),
+        ('39b77a2f-7398-4d5f-b744-cdcefca12671', 'd3abc217-75ee-46e9-a010-368f30282367', 'BXI', 'nobody-bxi@digital.justice.gov.uk', null),
 
-       ('20f3abc8-dd92-43ae-b88e-5797a0ad3f4b', '28e47d30-30bf-4dab-a8eb-9fda3f6400e8', 'OCC', 'nobody-iry@digital.justice.gov.uk', 'nobody2-iry@digital.justice.gov.uk'),
+       ('d460428c-5cb8-4d73-a3ae-b7ac37b65fbc', '1811faa6-d568-4fc4-83ce-41118b902421', 'BWN', 'nobody-bwn@digital.justice.gov.uk', null),
+       ('7fffcc6a-11f8-4713-be35-cf5ff1aee517', 'd3abc217-75ee-46e9-a010-368f30282367', 'MDI', 'nobody-mdi@digital.justice.gov.uk', 'nobody2-mdi@digital.justice.gov.uk'),
+       ('7f98826a-616c-4414-a278-525fc02505a0', 'd3abc217-75ee-46e9-a010-368f30282368', 'MDI', 'nobody-mdi@digital.justice.gov.uk', 'nobody2-mdi@digital.justice.gov.uk'),
+       ('fee62dde-87f5-4dfd-9a44-e80d48f64be9', 'd3abc217-75ee-46e9-a010-368f30282369', 'MDI', 'nobody-mdi@digital.justice.gov.uk', 'nobody2-mdi@digital.justice.gov.uk'),
+       ('77632e62-135c-4848-a5b9-d3dc6acfc690', 'd3abc217-75ee-46e9-a010-368f30282370', 'MDI', 'nobody-mdi@digital.justice.gov.uk', 'nobody2-mdi@digital.justice.gov.uk');
 
-       ('b328ebc8-1f7b-4236-b4ac-30f50b43a92d', '1811faa6-d568-4fc4-83ce-41118b90242e', 'BWN', 'nobody-bwn@digital.justice.gov.uk', null);
-
--- Add link table values
-
-INSERT INTO course_audience (course_id, audience_id)
-VALUES ('28e47d30-30bf-4dab-a8eb-9fda3f6400e8', '7fffcc6a-11f8-4713-be35-cf5ff1aee517');
+INSERT INTO referral (referral_id, offering_id, prison_number, referrer_id, additional_information, oasys_confirmed, has_reviewed_programme_history, status)
+VALUES
+    ('0c46ed09-170b-4c0f-aee8-a24eeaeeddaa', 'd460428c-5cb8-4d73-a3ae-b7ac37b65fbc','1uXTfdH','038019','',false,false,'REFERRAL_STARTED'),
+    ('fae2ed00-057e-4179-9e55-f6a4f4874cf0', 'd460428c-5cb8-4d73-a3ae-b7ac37b65fbc','1uXTfdH','038019','',false,false,'REFERRAL_STARTED');
