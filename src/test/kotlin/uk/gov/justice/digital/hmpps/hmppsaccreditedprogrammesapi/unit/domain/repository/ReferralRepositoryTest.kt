fba55dc9f791420a7a57e51533268c06cd61223d
Nick Johnson <nick.johnson@dxw.com>
2023-12-05 08:33:53 +0000

    Update ReferralRepositoryTest to account for new changes to Referral entity management:  - Use factory patterns to build entities instead of direct initialisation  - Use entity manager instead of direct persistence  - Remove unnecessary helper functions

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/unit/domain/repository/ReferralRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/unit/domain/repository/ReferralRepositoryTest.kt
@@ -2,100 +2,37 @@ package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.unit.domain.re
 
 import io.kotest.matchers.optional.shouldBePresent
 import io.kotest.matchers.shouldBe
+import jakarta.persistence.EntityManager
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.CLIENT_USERNAME
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.PRISON_NUMBER
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.REFERRER_ID
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.randomEmailAddress
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.randomLowercaseString
-import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.randomPrisonNumber
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.randomSentence
-import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.randomUppercaseAlphanumericString
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.randomUppercaseString
-import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.repository.CourseRepository
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.repository.ReferralRepository
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.unit.domain.entity.factory.CourseEntityFactory
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.unit.domain.entity.factory.OfferingEntityFactory
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.unit.domain.entity.factory.ReferralEntityFactory
-import java.util.UUID
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.unit.domain.entity.factory.ReferrerUserEntityFactory
 
 class ReferralRepositoryTest
 @Autowired
 constructor(
   val referralRepository: ReferralRepository,
-  val courseRepository: CourseRepository,
   jdbcTemplate: JdbcTemplate,
 ) : RepositoryTestBase(jdbcTemplate) {
-  @Test
-  fun `JpaReferralRepository should save and retrieve ReferralEntity objects`() {
-    val persistentOfferingId = persistAnOffering()
-    val prisonNumber = randomPrisonNumber()
-    val referrerId = randomUppercaseAlphanumericString(10)
 
-    val offering = OfferingEntityFactory()
-      .withId(persistentOfferingId)
-      .produce()
-
-    val referral = ReferralEntityFactory()
-      .withReferrerId(referrerId)
-      .withPrisonNumber(prisonNumber)
-      .withOffering(offering)
-      .produce()
-
-    val referralId = referralRepository.save(referral).id!!
-
-    commitAndStartNewTx()
-
-    referralRepository.findById(referralId) shouldBePresent {
-      referrerId shouldBe referrerId
-      prisonNumber shouldBe prisonNumber
-      offering shouldBe offering
-      oasysConfirmed shouldBe false
-      hasReviewedProgrammeHistory shouldBe false
-    }
-  }
+  @Autowired
+  private lateinit var entityManager: EntityManager
 
   @Test
-  fun `JpaReferralRepository should update and retrieve ReferralEntity objects`() {
-    val persistentOfferingId = persistAnOffering()
-    val prisonNumber = randomPrisonNumber()
-    val referrerId = randomUppercaseAlphanumericString(10)
-
-    val offering = OfferingEntityFactory()
-      .withId(persistentOfferingId)
-      .produce()
-
-    val referral = ReferralEntityFactory()
-      .withReferrerId(referrerId)
-      .withPrisonNumber(prisonNumber)
-      .withOffering(offering)
-      .produce()
-
-    val referralId = referralRepository.save(referral).id!!
-
-    commitAndStartNewTx()
-
-    val persistentReferral = referralRepository.findById(referralId).get()
-    with(persistentReferral) {
-      oasysConfirmed = true
-      hasReviewedProgrammeHistory = true
-    }
-
-    commitAndStartNewTx()
-
-    referralRepository.findById(referralId) shouldBePresent {
-      referrerId shouldBe referrerId
-      prisonNumber shouldBe prisonNumber
-      offering shouldBe offering
-      oasysConfirmed shouldBe true
-      hasReviewedProgrammeHistory shouldBe true
-    }
-  }
-
-  private fun persistAnOffering(): UUID {
-    val courseIdentifier = randomLowercaseString(6)
-
+  fun `ReferralRepository should save and retrieve ReferralEntity objects`() {
     val course = CourseEntityFactory()
-      .withIdentifier(courseIdentifier)
+      .withIdentifier(randomLowercaseString(6))
       .withName(randomSentence(1..3, 1..8))
       .withAlternateName(null)
       .produce()
@@ -105,8 +42,77 @@ constructor(
       .withContactEmail(randomEmailAddress())
       .produce()
 
-    courseRepository.saveCourse(course.apply { addOffering(offering) })
-    commitAndStartNewTx()
-    return courseRepository.getAllCourses()[0].offerings.first().id!!
+    entityManager.merge(course.apply { addOffering(offering) })
+
+    val referrer = ReferrerUserEntityFactory()
+      .withUsername(CLIENT_USERNAME)
+      .produce()
+
+    entityManager.persist(referrer)
+
+    val referral = ReferralEntityFactory()
+      .withReferrer(referrer)
+      .withReferrerId(REFERRER_ID)
+      .withPrisonNumber(PRISON_NUMBER)
+      .withOffering(offering)
+      .produce()
+
+    entityManager.merge(referral)
+
+    referralRepository.findById(referral.id!!) shouldBePresent {
+      referrer shouldBe referrer
+      referrerId shouldBe referrerId
+      prisonNumber shouldBe prisonNumber
+      offering shouldBe offering
+      offering.course shouldBe course
+      oasysConfirmed shouldBe false
+      hasReviewedProgrammeHistory shouldBe false
+    }
+  }
+
+  @Test
+  fun `ReferralRepository should update and retrieve ReferralEntity objects`() {
+    val course = CourseEntityFactory()
+      .withIdentifier(randomLowercaseString(6))
+      .withName(randomSentence(1..3, 1..8))
+      .withAlternateName(null)
+      .produce()
+
+    val offering = OfferingEntityFactory()
+      .withOrganisationId(randomUppercaseString(3))
+      .withContactEmail(randomEmailAddress())
+      .produce()
+
+    entityManager.merge(course.apply { addOffering(offering) })
+
+    val referrer = ReferrerUserEntityFactory()
+      .withUsername(CLIENT_USERNAME)
+      .produce()
+
+    entityManager.persist(referrer)
+
+    val referral = ReferralEntityFactory()
+      .withReferrer(referrer)
+      .withReferrerId(REFERRER_ID)
+      .withPrisonNumber(PRISON_NUMBER)
+      .withOffering(offering)
+      .produce()
+
+    entityManager.merge(referral)
+
+    referral.oasysConfirmed = true
+    referral.hasReviewedProgrammeHistory = true
+
+    entityManager.merge(referral)
+
+    referralRepository.findById(referral.id!!) shouldBePresent {
+      referrer shouldBe referrer
+      referrerId shouldBe referrerId
+      prisonNumber shouldBe prisonNumber
+      offering shouldBe offering
+      offering.course shouldBe course
+      oasysConfirmed shouldBe true
+      hasReviewedProgrammeHistory shouldBe true
+    }
   }
 }
