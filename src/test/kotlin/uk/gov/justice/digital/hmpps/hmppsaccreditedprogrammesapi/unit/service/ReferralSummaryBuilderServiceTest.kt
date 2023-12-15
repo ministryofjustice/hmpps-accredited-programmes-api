a1b17354be14a26e19cf25caf0d7157dff779ace
Nick Johnson <nick.johnson@dxw.com>
2023-12-15 16:04:34 +0000

    Update ReferralSummaryBuilderServiceTest to include testing both true and false values for includeEarliestReleaseDate. Move package for ReferralSummaryBuilderServiceTest to align with src/main/kotlin architecture.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/unit/service/ReferralSummaryBuilderServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/unit/service/ReferralSummaryBuilderServiceTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.service
+package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.unit.service
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -21,12 +21,13 @@ import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.TAR
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.util.WHITE_COURSE
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.entity.create.ReferralEntity
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.projection.ReferralSummaryProjection
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.service.ReferralSummaryBuilderService
 import java.time.LocalDateTime
 import java.util.*
 
 class ReferralSummaryBuilderServiceTest {
 
-  val service = ReferralSummaryBuilderService()
+  private val referralSummaryBuilderService = ReferralSummaryBuilderService()
 
   companion object {
     private val referralSummaryProjection1 = ReferralSummaryProjection(
@@ -51,9 +52,9 @@ class ReferralSummaryBuilderServiceTest {
   }
 
   @Test
-  fun `build referral summary successful`() {
+  fun `Building a referral summary requiring all sentence information should succeed`() {
     val referralSummaries =
-      service.build(listOf(referralSummaryProjection1, referralSummaryProjection2), PRISONERS, PRISONS, ORGANISATION_ID_MDI)
+      referralSummaryBuilderService.build(listOf(referralSummaryProjection1, referralSummaryProjection2), PRISONERS, PRISONS, ORGANISATION_ID_MDI, false)
 
     assertEquals(2, referralSummaries.size)
 
@@ -70,6 +71,7 @@ class ReferralSummaryBuilderServiceTest {
       sentence?.nonDtoReleaseDateType shouldBe NON_DTO_RELEASE_DATE_TYPE
       sentence?.tariffExpiryDate shouldBe TARIFF_EXPIRY_DATE
       sentence?.paroleEligibilityDate shouldBe PAROLE_ELIGIBILITY_DATE
+      earliestReleaseDate shouldBe null
     }
 
     with(referralSummaries[1]) {
@@ -85,6 +87,47 @@ class ReferralSummaryBuilderServiceTest {
       sentence?.nonDtoReleaseDateType shouldBe NON_DTO_RELEASE_DATE_TYPE
       sentence?.tariffExpiryDate shouldBe TARIFF_EXPIRY_DATE
       sentence?.paroleEligibilityDate shouldBe PAROLE_ELIGIBILITY_DATE
+      earliestReleaseDate shouldBe null
+    }
+  }
+
+  @Test
+  fun `Building a referral summary requiring only earliest release date should succeed`() {
+    val referralSummaries =
+      referralSummaryBuilderService.build(listOf(referralSummaryProjection1, referralSummaryProjection2), PRISONERS, PRISONS, ORGANISATION_ID_MDI, true)
+
+    assertEquals(2, referralSummaries.size)
+
+    with(referralSummaries[0]) {
+      organisationId shouldBe ORGANISATION_ID_MDI
+      id shouldBe referralSummaryProjection1.referralId
+      courseName shouldBe INDIGO_COURSE
+      prisonNumber shouldBe PRISON_NUMBER
+      prisonName shouldBe PRISON_NAME
+      prisonerName?.firstName shouldBe PRISONER_FIRST_NAME
+      prisonerName?.lastName shouldBe PRISONER_LAST_NAME
+      sentence?.indeterminateSentence shouldBe INDETERMINATE_SENTENCE
+      sentence?.conditionalReleaseDate shouldBe CONDITIONAL_RELEASE_DATE
+      sentence?.nonDtoReleaseDateType shouldBe NON_DTO_RELEASE_DATE_TYPE
+      sentence?.tariffExpiryDate shouldBe TARIFF_EXPIRY_DATE
+      sentence?.paroleEligibilityDate shouldBe PAROLE_ELIGIBILITY_DATE
+      earliestReleaseDate shouldBe PAROLE_ELIGIBILITY_DATE
+    }
+
+    with(referralSummaries[1]) {
+      organisationId shouldBe ORGANISATION_ID_MDI
+      id shouldBe referralSummaryProjection2.referralId
+      courseName shouldBe WHITE_COURSE
+      prisonNumber shouldBe PRISON_NUMBER
+      prisonName shouldBe PRISON_NAME
+      prisonerName?.firstName shouldBe PRISONER_FIRST_NAME
+      prisonerName?.lastName shouldBe PRISONER_LAST_NAME
+      sentence?.indeterminateSentence shouldBe INDETERMINATE_SENTENCE
+      sentence?.conditionalReleaseDate shouldBe CONDITIONAL_RELEASE_DATE
+      sentence?.nonDtoReleaseDateType shouldBe NON_DTO_RELEASE_DATE_TYPE
+      sentence?.tariffExpiryDate shouldBe TARIFF_EXPIRY_DATE
+      sentence?.paroleEligibilityDate shouldBe PAROLE_ELIGIBILITY_DATE
+      earliestReleaseDate shouldBe PAROLE_ELIGIBILITY_DATE
     }
   }
 }
