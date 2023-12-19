64b34101d633b00b919508bf590ab5fb82795094
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2023-12-19 10:18:17 +0000

    fix flyway referrer_user_username_fk

--- src/main/resources/db/migration/V26__retire_referrerId.sql
+++ src/main/resources/db/migration/V26__retire_referrerId.sql
@@ -1,8 +1,3 @@
--- Always ensure that referrals have an associated user, even if that user is a placeholder for now.
-UPDATE referral
-SET referrer_username = 'UNKNOWN_USER'
-WHERE referrer_username IS NULL OR referrer_username = '';
-
 
 -- Ensure data integrity in the referrer_user table by adding new unique users which don't already exist to it.
 INSERT INTO referrer_user (referrer_username)
@@ -14,6 +9,14 @@ WHERE r.referrer_username IS NOT NULL
     FROM referrer_user ru
     WHERE ru.referrer_username = r.referrer_username
 );
+
+INSERT INTO referrer_user(referrer_username) VALUES ('UNKNOWN_USER');
+
+-- Always ensure that referrals have an associated user, even if that user is a placeholder for now.
+UPDATE referral
+SET referrer_username = 'UNKNOWN_USER'
+WHERE referrer_username IS NULL OR referrer_username = '';
+
 
 -- Delete the old field.
 ALTER TABLE referral DROP COLUMN referrer_id;
