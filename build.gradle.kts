eb107d126522135ec49705365d4e635d3fc747fb
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2024-02-08 07:19:34 +0000

    :jackson-dataformat-csv:2.15.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.flywaydb:flyway-core")
-  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.2")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.3")
   implementation("com.google.guava:guava:33.0.0-jre")
 
   implementation("io.sentry:sentry-spring-boot-starter-jakarta:$sentryVersion")
