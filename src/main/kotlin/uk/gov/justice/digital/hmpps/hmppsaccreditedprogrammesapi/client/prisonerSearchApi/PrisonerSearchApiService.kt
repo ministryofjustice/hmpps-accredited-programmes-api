ed66c7d9548d8613663ae55473d07bab24f23ff0
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-02-12 17:56:01 +0000

    updated following review

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/client/prisonerSearchApi/PrisonerSearchApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/client/prisonerSearchApi/PrisonerSearchApiService.kt
@@ -1,11 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.client.prisonerSearchApi
+
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.api.model.PrisonerSearchRequest
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.client.AuthorisableActionResult
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.client.ClientResult
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.client.prisonerSearchApi.model.Prisoner
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.client.prisonerSearchApi.model.PrisonerSearchResponse
 
 @Service
 @Transactional
@@ -23,12 +26,29 @@ constructor(
       is ClientResult.Success -> AuthorisableActionResult.Success(response.body)
       is ClientResult.Failure.StatusCode -> {
         log.warn("Failure to retrieve data. Status code ${response.status}")
-        AuthorisableActionResult.Success(listOf<Prisoner>())
+        AuthorisableActionResult.Success(emptyList())
       }
 
       is ClientResult.Failure -> {
         log.warn("Failure to retrieve data")
-        AuthorisableActionResult.Success(listOf<Prisoner>())
+        AuthorisableActionResult.Success(emptyList())
+      }
+    }
+
+    return prisoners.entity
+  }
+
+  fun searchPrisoners(prisonerSearchRequest: PrisonerSearchRequest): List<PrisonerSearchResponse> {
+    val prisoners = when (val response = prisonerSearchApiClient.prisonerSearch(prisonerSearchRequest)) {
+      is ClientResult.Success -> AuthorisableActionResult.Success(response.body)
+      is ClientResult.Failure.StatusCode -> {
+        log.warn("Failure to retrieve data. Status code ${response.status}")
+        AuthorisableActionResult.Success(emptyList())
+      }
+
+      is ClientResult.Failure -> {
+        log.warn("Failure to retrieve data")
+        AuthorisableActionResult.Success(emptyList())
       }
     }
 
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/integration/PrisonSearchIntegrationTest.kt
@@ -0,0 +1,61 @@
+package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.integration
+
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.nulls.shouldNotBeNull
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.expectBody
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.api.model.PrisonerSearchRequest
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.api.model.PrisonerSearchResponse
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.config.JwtAuthHelper
+import java.time.LocalDate
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Import(JwtAuthHelper::class)
+class PrisonSearchIntegrationTest : IntegrationTestBase() {
+
+  @Test
+  fun `search for a prisoner by prisonId`() {
+    mockClientCredentialsJwtRequest(jwt = jwtAuthHelper.bearerToken())
+
+    val prisonerSearchRequest = PrisonerSearchRequest("C6666DD", listOf("MDI"))
+    val response = searchPrisoners(prisonerSearchRequest)
+
+    response.shouldNotBeNull()
+    response.first() shouldBeEqual PrisonerSearchResponse(
+      bookingId = "1202335",
+      conditionalReleaseDate = null,
+      prisonName = "Nottingham (HMP)",
+      dateOfBirth = LocalDate.of(1975, 1, 1),
+      ethnicity = "White: Eng./Welsh/Scot./N.Irish/British",
+      gender = "Male",
+      homeDetentionCurfewEligibilityDate = null,
+      indeterminateSentence = false,
+      firstName = "MICKEY",
+      lastName = "SMITH",
+      paroleEligibilityDate = null,
+      prisonerNumber = "C6666DD",
+      religion = null,
+      sentenceExpiryDate = null,
+      sentenceStartDate = null,
+      tariffDate = null,
+    )
+  }
+
+  fun searchPrisoners(prisonerSearchRequest: PrisonerSearchRequest) =
+    webTestClient
+      .post()
+      .uri("/prisoner-search")
+      .header(HttpHeaders.AUTHORIZATION, jwtAuthHelper.bearerToken())
+      .accept(MediaType.APPLICATION_JSON)
+      .bodyValue(prisonerSearchRequest)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody<List<PrisonerSearchResponse>>()
+      .returnResult().responseBody!!
+}
