94582d016eedf919fcd12348569d7380fbe8dd2e
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2024-03-05 10:00:07 +0000

    1431 - Create new endpoint returning enabled organisations(pilot sites)

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/integration/EnabledOrganisationsIntegrationTest.kt
@@ -0,0 +1,45 @@
+package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.integration
+
+import io.kotest.matchers.equals.shouldBeEqual
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.common.config.JwtAuthHelper
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.repository.EnabledOrganisationRepository
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.service.EnabledOrganisationService
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.unit.domain.entity.factory.EnabledOrganisationEntityFactory
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Import(JwtAuthHelper::class)
+class EnabledOrganisationsIntegrationTest : IntegrationTestBase() {
+
+  @Autowired
+  lateinit var enabledOrganisationRepository: EnabledOrganisationRepository
+
+  @Autowired
+  lateinit var enabledOrganisationService: EnabledOrganisationService
+
+  @BeforeEach
+  fun setUp() {
+    enabledOrganisationRepository.deleteAll()
+  }
+
+  @Test
+  fun `should return list of enabled organisations`() {
+    val code = "ONI"
+    val desc = "Onley"
+
+    val enabledOrganisation = EnabledOrganisationEntityFactory().code(code).description(desc).produce()
+    enabledOrganisationRepository.save(enabledOrganisation)
+
+    val enabledOrganisations = enabledOrganisationService.getEnabledOrganisations()
+
+    enabledOrganisations.isNotEmpty()
+    enabledOrganisations[0].code shouldBeEqual code
+    enabledOrganisations[0].description shouldBeEqual desc
+  }
+}
