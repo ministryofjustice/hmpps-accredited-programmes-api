f1f0b56df0647aa79d789248664613316383fff1
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-04-05 11:26:27 +0100

    additional status transition text

---
+++ src/main/resources/db/migration/V64__fixes_for_on_programme.sql
@@ -0,0 +1,11 @@
+update referral_status_transitions set primary_heading = 'Deselect person: assessed as suitable' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'ASSESSED_SUITABLE';
+update referral_status_transitions set primary_description = 'This person cannot complete the programme now. They may be able to join or restart when the programme runs again.' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'ASSESSED_SUITABLE';
+update referral_status_transitions set secondary_heading = 'Give a reason' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'ASSESSED_SUITABLE';
+update referral_status_transitions set secondary_description = 'Give more information about the reason for deselecting this person.' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'ASSESSED_SUITABLE';
+update referral_status_transitions set warning_text = 'Submitting this will change the status to assessed as suitable.' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'ASSESSED_SUITABLE';
+
+update referral_status_transitions set primary_heading = 'Deselect person: suitable but not ready' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'SUITABLE_NOT_READY';
+update referral_status_transitions set primary_description = 'This person cannot complete the programme now. The referral will be paused until the person is ready to continue.' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'SUITABLE_NOT_READY';
+update referral_status_transitions set secondary_heading = 'Give a reason' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'SUITABLE_NOT_READY';
+update referral_status_transitions set secondary_description = 'Give more information about the reason for deselecting this person.' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'SUITABLE_NOT_READY';
+update referral_status_transitions set warning_text = 'Submitting this will change the status to suitable not ready.' where transition_from_status = 'ON_PROGRAMME' and transition_to_status = 'SUITABLE_NOT_READY';
