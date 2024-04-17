300a26b0895dabd400898d557c2b30074edb5413
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-04-17 14:01:58 +0100

    WIP commit

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/integration/DomainEventsListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/integration/DomainEventsListenerTest.kt
@@ -8,18 +8,18 @@ import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.listener.Identi
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.listener.PersonReference
 
 class DomainEventsListenerTest : IntegrationTestBase() {
-    @Test
-    fun `update offender message`() {
-        val eventType = "prisoner-offender-search.prisoner.updated"
-        val crn = "B123123B"
-        val nomsNumber = "N321321"
-        sendDomainEvent(
-            DomainEventsMessage(
-                eventType,
-                PersonReference(listOf(Identifiers("CRN", crn))),
-               additionalInformation = mapOf("nomsNumber" to nomsNumber),
-            )
-        )
-        verify(referralService, timeout(5000)).updatePerson(nomsNumber)
-    }
+  @Test
+  fun `update offender message`() {
+    val eventType = "prisoner-offender-search.prisoner.updated"
+    val crn = "B123123B"
+    val nomsNumber = "N321321"
+    sendDomainEvent(
+      DomainEventsMessage(
+        eventType,
+        PersonReference(listOf(Identifiers("CRN", crn))),
+        additionalInformation = mapOf("nomsNumber" to nomsNumber),
+      ),
+    )
+    verify(referralService, timeout(5000)).updatePerson(nomsNumber)
+  }
 }
