4b18c3c976252746db1b45d49d493aa897e1c2d5
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-04-23 17:20:43 +0100

    adding test back in

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/integration/DomainEventsListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/integration/DomainEventsListenerTest.kt
@@ -1,28 +1,71 @@
 package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.integration
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.timeout
 import org.mockito.kotlin.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.test.context.ActiveProfiles
+import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.listener.DomainEventsMessage
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.listener.SQSMessage
 import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.service.ReferralService
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.MissingQueueException
 import uk.gov.justice.hmpps.sqs.countMessagesOnQueue
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = ["test", "domain-events"])
-class DomainEventsListenerTest : IntegrationTestBase() {
+class DomainEventsListenerTest {
 
   @SpyBean
   lateinit var referralService: ReferralService
 
+  @Autowired
+  lateinit var hmppsQueueService: HmppsQueueService
+
+  val domainEventQueue by lazy {
+    hmppsQueueService.findByQueueId("hmppsdomaineventsqueue")
+      ?: throw MissingQueueException("HmppsQueue hmppsdomaineventsqueue not found")
+  }
+  val domainEventQueueDlqClient by lazy { domainEventQueue.sqsDlqClient }
+  val domainEventQueueClient by lazy { domainEventQueue.sqsClient }
+  val objectMapper = jacksonObjectMapper().apply {
+    registerModule(JavaTimeModule())
+  }
+
+  @BeforeEach
+  fun beforeEach() {
+    domainEventQueueClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(domainEventQueue.queueUrl).build()).get()
+    domainEventQueueDlqClient!!.purgeQueue(PurgeQueueRequest.builder().queueUrl(domainEventQueue.dlqUrl).build())
+      .get()
+  }
+
+  fun sendDomainEvent(
+    message: DomainEventsMessage,
+    queueUrl: String = domainEventQueue.queueUrl,
+  ): SendMessageResponse = domainEventQueueClient.sendMessage(
+    SendMessageRequest.builder()
+      .queueUrl(queueUrl)
+      .messageBody(
+        objectMapper.writeValueAsString(SQSMessage(objectMapper.writeValueAsString(message))),
+      ).build(),
+  ).get()
+
   @Test
   fun `update offender message`() {
     val eventType = "prisoner-offender-search.prisoner.updated"
-    val nomsNumber = "N321321"
+    val nomsNumber = "C6666CC"
     sendDomainEvent(
       DomainEventsMessage(
         eventType,
