92a25f2a334719da75f782701b9362dabe428528
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-05-30 11:29:43 +0100

    added sorting to key dates

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/service/PersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/service/PersonService.kt
@@ -87,6 +87,7 @@ class PersonService(
           ),
         )
       }
+      return keyDates
     }
 
     // now find the earliest of these dates:
@@ -109,39 +110,39 @@ class PersonService(
   }
 
   enum class KeyDateType(val mapping: String, val code: String, val description: String, val order: Int = 1) {
-    ACTUAL_PAROLE_DATE("actualParoleDate", "APD", "Approved parole date", 1),
-    AUTOMATIC_RELEASE_DATE("automaticReleaseDate", "ARD", "Automatic release date", 2),
-    CONDITIONAL_RELEASE_DATE("conditionalReleaseDate", "CRD", "Conditional release date", 3),
+    ACTUAL_PAROLE_DATE("actualParoleDate", "APD", "Approved parole date", 10),
+    AUTOMATIC_RELEASE_DATE("automaticReleaseDate", "ARD", "Automatic release date", 20),
+    CONDITIONAL_RELEASE_DATE("conditionalReleaseDate", "CRD", "Conditional release date", 30),
     EARLY_REMOVAL_SCHEME_ELIGIBILITY_DATE(
       "earlyRemovalSchemeEligibilityDate",
       "ERSED",
       "Early removal scheme eligibility date",
-      4,
+      40,
     ),
     HOME_DETENTION_CURFEW_ACTUAL_DATE(
       "homeDetentionCurfewActualDate",
       "HDCAD",
       "Home detention curfew approved date",
-      5,
+      50,
     ),
     HOME_DETENTION_CURFEW_ELIGIBILITY_DATE(
       "homeDetentionCurfewEligibilityDate",
       "HDCED",
       "Home detention curfew eligibility date",
-      6,
+      60,
     ),
-    MID_TERM_DATE("midTermDate", "MTD", "Mid term date", 7),
-    NON_PAROLE_DATE("nonParoleDate", "NPD", "Non-parole date", 8),
+    MID_TERM_DATE("midTermDate", "MTD", "Mid term date", 70),
+    NON_PAROLE_DATE("nonParoleDate", "NPD", "Non-parole date", 80),
     TARIFF_EARLY_REMOVAL_SCHEME_ELIGIBILITY_DATE(
       "tariffEarlyRemovalSchemeEligibilityDate",
       "TERSED",
       "Tariff expired release scheme eligibility date",
-      9,
+      90,
     ),
-    TARIFF_DATE("tariffDate", "TED", "Tariff Expiry Date", 10),
-    PAROLE_ELIGIBILITY_DATE("paroleEligibilityDate", "PED", "Parole eligibility date", 11),
-    POST_RECALL_RELEASE_DATE("dtoPostRecallReleaseDate", "PRRD", "Post recall release date", 12),
-    RELEASE_DATE("releaseDate", "RD", "Release date", 13),
+    TARIFF_DATE("tariffDate", "TED", "Tariff Expiry Date", 100),
+    PAROLE_ELIGIBILITY_DATE("paroleEligibilityDate", "PED", "Parole eligibility date", 110),
+    POST_RECALL_RELEASE_DATE("dtoPostRecallReleaseDate", "PRRD", "Post recall release date", 120),
+    RELEASE_DATE("releaseDate", "RD", "Release date", 130),
     ;
 
     companion object {
