5f93f26bac2421163075e117b6cf88215f79f8d1
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-08-01 15:43:43 +0100

    added missing fk constraints

---
+++ src/main/resources/db/migration/V85__add_missing_fk.sql
@@ -0,0 +1,27 @@
+ALTER TABLE referral
+    ADD CONSTRAINT r_status_fk
+        FOREIGN KEY (status) REFERENCES referral_status(code);
+
+ALTER TABLE referral_status_history
+    ADD CONSTRAINT rs_referral_fk
+        FOREIGN KEY (referral_id) REFERENCES referral(referral_id);
+
+ALTER TABLE referral_status_history
+    ADD CONSTRAINT rs_status_fk
+        FOREIGN KEY (status) REFERENCES referral_status(code);
+
+ALTER TABLE referral_status_history
+    ADD CONSTRAINT rs_previous_status_fk
+        FOREIGN KEY (previous_status) REFERENCES referral_status(code);
+
+ALTER TABLE referral_status_history
+    ADD CONSTRAINT rs_username_fk
+        FOREIGN KEY (username) REFERENCES referrer_user(referrer_username);
+
+ALTER TABLE referral_status_history
+    ADD CONSTRAINT rs_category_fk
+        FOREIGN KEY (category) REFERENCES referral_status_category(code);
+
+ALTER TABLE referral_status_history
+    ADD CONSTRAINT rs_reason_fk
+        FOREIGN KEY (reason) REFERENCES referral_status_reason(code);
