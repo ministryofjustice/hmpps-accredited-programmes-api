5872d0c2ff295ff79367914e02437cc782aeac70
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2024-10-14 14:47:28 +0100

    Add SentenceCategoryEntity for sentence category management

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/domain/entity/referencedata/SentenceCategoryEntity.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.entity.referencedata
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.entity.referencedata.type.SentenceCategoryType
+
+@Entity
+@Table(name = "sentence_category")
+data class SentenceCategoryEntity(
+  @Id
+  val description: String,
+  @Enumerated(EnumType.STRING)
+  val category: SentenceCategoryType,
+)
