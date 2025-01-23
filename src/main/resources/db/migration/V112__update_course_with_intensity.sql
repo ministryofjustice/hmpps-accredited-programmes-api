89074e82007d7abd075a6fd99683d6e0d0d32a04
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2025-01-23 08:53:03 +0000

    APG-629 Return courses with matching intensity

--- src/main/resources/db/migration/V112__update_course_with_intensity.sql
+++ src/main/resources/db/migration/V112__update_course_with_intensity.sql
@@ -8,7 +8,8 @@ UPDATE COURSE SET intensity = 'HIGH' WHERE name IN ( 'Kaizen', 'Building Choices
 
 
 UPDATE COURSE SET intensity = 'MODERATE' WHERE name IN ('Thinking Skills Programme', 'Building Choices: moderate intensity', 'New Me Strengths',
-                                                        'Building Better Relationships', 'Horizon', 'Control of Violence for Angry Impulsive Drinkers',
+                                                        'Building Better Relationships', 'Horizon', 'iHorizon', 'Control of Violence for Angry Impulsive Drinkers',
                                                         'Identity Matters');
 
 UPDATE COURSE SET intensity = 'HIGH MODERATE' WHERE name IN ('New Me MOT', 'Living as New Me');
+
