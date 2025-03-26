c2d9a42a7d7cbe9cf94508e85a9b2baff33b6f8b
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-03-26 18:46:09 +0000

    [APG-808] Remove spurious nullable in column definition

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/domain/entity/create/CourseEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/domain/entity/create/CourseEntity.kt
@@ -22,7 +22,7 @@ import java.util.UUID
 @Table(name = "course")
 class CourseEntity(
   @Id
-  @Column(name = "course_id", nullable = true)
+  @Column(name = "course_id")
   val id: UUID? = null,
 
   @Version
