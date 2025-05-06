d070117c08c2fca0bcc17342ab7c0016dbb5535f
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-05-06 11:09:53 +0100

    [APG-884] Add SexualOffenceEntity and related category types
    Introduces `SexualOffenceEntity` and `SexualOffenceCategoryType` to model sexual offence data. Includes a new migration script for inserting referral status categories and reasons to support HSP categorization. These changes enable enriched data handling for sexual offence assessment.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsaccreditedprogrammesapi/domain/entity/referencedata/SexualOffenceEntity.kt
@@ -0,0 +1,32 @@
+package uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.entity.referencedata
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import uk.gov.justice.digital.hmpps.hmppsaccreditedprogrammesapi.domain.entity.referencedata.type.SexualOffenceCategoryType
+import java.util.*
+
+@Entity
+@Table(name = "sexual_offence")
+class SexualOffenceEntity(
+
+    @Id
+    @Column(name = "id")
+    var id: UUID? = null,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    val category: SexualOffenceCategoryType,
+
+    @Column(name = "description")
+    val description: String,
+
+    @Column(name = "hint_text")
+    val hintText: String,
+
+    @Column(name = "score")
+    val score: Int
+)
